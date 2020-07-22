d135cafc915625ad257059e6ac80a5158b5ab33f
shoaib ahmed <ashoab@ymail.com>
2020-07-22 11:09:09 +0600

    Update view.blade.php

--- view.blade.php
+++ view.blade.php
@@ -1,5 +1,26 @@
   
-  
+  <div class="row">
+
+			<div class="col-sm-12">
+
+				<div class="box-info">
+
+					<h2><strong>{!! trans('Employee') !!}</strong> {!! trans('Statistic') !!}</h2>
+
+					<div id="website-statistic" class="statistic-chart collapse in">
+
+						<div id="morris-home" style="height: 200px;"></div>
+
+					</div>
+
+				</div>
+
+				
+
+			</div>
+
+		</div>
+
   
   <script type="text/javascript">
     
